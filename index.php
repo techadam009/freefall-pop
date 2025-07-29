c25c55bf003a09b597f686065cf8f359dbbdb6fb
techadam009 <136915069+techadam009@users.noreply.github.com>
2025-07-29 18:53:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 
 <xmlns="http:/www.w3.org/1999/xhtml">
 <head>
-
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T4QCVTDW');</script>
+<!-- End Google Tag Manager -->
 
 
 
@@ -68,7 +74,10 @@ if (window.navigator.userAgent.indexOf("Opera") != -1) {
 
 </head>
 <body>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T4QCVTDW"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 </body>
 
 
